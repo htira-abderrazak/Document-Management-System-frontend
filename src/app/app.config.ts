07719a7e514bec25e4ff8a20c9266f044c014116
htira-abderrazak <abderrazak.htira@gmail.com>
2024-07-22 14:26:53 +0100

    refactore : delete unnecessary code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,19 +3,24 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { environment } from '../environments/environment';
 import { RECAPTCHA_V3_SITE_KEY } from 'ng-recaptcha';
-import { authInterceptor } from './auth/interceptor/auth.interceptor';
+import { TokenRefreshInterceptor } from './auth/TokenRefreshInterceptor/token-refresh-interceptor.interceptor';
+import { authInterceptor } from './auth/AuthInterceptor/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideClientHydration(),
-    provideHttpClient(withFetch(),withInterceptors([authInterceptor])
-  ),
+    provideHttpClient(withFetch(), withInterceptors([authInterceptor, TokenRefreshInterceptor])),
+    // provideHttpClient(withFetch(),withInterceptors([authInterceptor])),
+
     NgbActiveModal,
     provideAnimations(),
     { provide: RECAPTCHA_V3_SITE_KEY, useValue: environment.recaptcha.siteKey },
