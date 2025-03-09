9db5ba8b0390b20a35cbd341848abb9f41caabec
htira-abderrazak <abderrazak.htira@gmail.com>
2025-03-09 17:44:37 +0100

    chore  : change reaptcha key

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiUrl: 'https://dmsbackend.duckdns.org',
   recaptcha: {
-    siteKey: '6LcnqBQpAAAAABZrxISU06XNzPuBoDL0_Get_c6k',
+    siteKey: '6LcGKO8qAAAAAJQLF5oSEHSJSYRb9L1TbbBVJJ5f',
   },
   STRIPE_KEY:
     'pk_test_51PiGnDRplQeQR6kQaIMgNNwRCNQy28PS6Rm8uxjp1fAZ4HyUWtKsgyz9zSIKuR9mY6ZFVuDBMwGb5zhNQ3UIWuAA00gACCZoWT',
