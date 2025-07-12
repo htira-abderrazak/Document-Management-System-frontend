2a5d793dde97512fd75d16bdce422d1f3e0c145b
htira-abderrazak <abderrazak.htira@gmail.com>
2025-07-12 14:28:43 +0100

    feat : create a new chatbot library

---
+++ projects/my-chatbot-library/src/lib/my-chatbot-library.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-my-chatbot-library',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      my-chatbot-library works!
+    </p>
+  `,
+  styles: ``
+})
+export class MyChatbotLibraryComponent {
+
+}
---
+++ projects/my-chatbot-library/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of my-chatbot-library
+ */
+
+export * from './lib/my-chatbot-library.service';
+export * from './lib/my-chatbot-library.component';
