c43f6c8ec789999e7ab542e1998857b13b406c0c
htira-abderrazak <abderrazak.htira@gmail.com>
2025-07-14 14:10:19 +0100

    feat : export library apis

--- projects/my-chatbot-library/src/public-api.ts
+++ projects/my-chatbot-library/src/public-api.ts
@@ -4,3 +4,8 @@
 
 export * from './lib/my-chatbot-library.service';
 export * from './lib/my-chatbot-library.component';
+export * from './lib/chatbot-icon/chatbot-icon.component';
+export * from './lib/chatbot-textbox/chatbot-textbox.component';
+export * from './lib/directives/typing.directive';
+export * from './lib/interfaces/library.interface';
+
