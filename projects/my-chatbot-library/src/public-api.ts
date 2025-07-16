db355f94b9daab43aa3ee93de74b0cbb6e6a56de
htira-abderrazak <abderrazak.htira@gmail.com>
2025-07-16 17:16:23 +0100

    refactor : remove  service export

--- projects/my-chatbot-library/src/public-api.ts
+++ projects/my-chatbot-library/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of my-chatbot-library
  */
 
-export * from './lib/my-chatbot-library.service';
 export * from './lib/my-chatbot-library.component';
 export * from './lib/chatbot-icon/chatbot-icon.component';
 export * from './lib/chatbot-textbox/chatbot-textbox.component';
